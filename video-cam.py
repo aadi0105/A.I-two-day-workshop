de7c86f5591d40db53bce1cc4f6354f01420e3cf
aditya mishra <mishraaditya8782@gmail.com>
2022-01-14 23:52:54 +0530

    cropped-video feature added

--- video-cam.py
+++ video-cam.py
@@ -2,7 +2,9 @@ import cv2
 cap=cv2.VideoCapture(0)
 while True:
     status, photo=cap.read()
-    cv2.imshow('hii',photo)
+    #for giving cropped video of ypur face uncomment below line 
+    #newphoto = photo[120:680, 135:500]
+    cv2.imshow('hii',newphoto)
     if cv2.waitKey(1) == 27:
         break
 cv2.destroyAllWindows()
